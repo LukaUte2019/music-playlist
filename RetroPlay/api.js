12dead4935383821c1a99e7210ba085942a215db
LukaUte2019 <46568221+LukaUte2019@users.noreply.github.com>
2025-02-14 19:23:44 +0100

    retro play

--- RetroPlay/api.js
+++ RetroPlay/api.js
@@ -1,7 +1,8 @@
 const errorElement = document.getElementById('error');
+let loadOnlineAPI = false;
 
 $.ajax({
-	url: "http://lukaserver.ddns.net:81/retrodance",
+	url: "/songs.json",
 
 	success: function(data) {
 	errorElement.style.display = 'none';
@@ -12,15 +13,25 @@ $.ajax({
 	}
 })
 export async function getSongs() {
-	const response = await fetch('http://lukaserver.ddns.net:81/retrodance');
-	const songs = await response.json();
 
-
-
-	return new Promise((resolve) => {
-		const delay = Math.floor(Math.random() * 3000) + 1000;
-		setTimeout(() => {
-			resolve(songs);
-		}, delay)
-	})
-}
+	if (loadOnlineAPI) {
+		const response = await fetch('http://lukaserver.ddns.net:81/retrodance');
+		const songs = await response.json();
+		return new Promise((resolve) => {
+			const delay = Math.floor(Math.random() * 3000) + 1000;
+			setTimeout(() => {
+				resolve(songs);
+			}, delay)
+		})
+		
+	} else {
+		const response = await fetch('/RetroPlay/songs.json');
+		const songs = await response.json();
+		return new Promise((resolve) => {
+			const delay = Math.floor(Math.random() * 3000) + 1000;
+			setTimeout(() => {
+				resolve(songs);
+			}, delay)
+		})
+	}
+	}
