4dc295761f623945e53fde7ebceb368ee1217752
LukaUte2019 <46568221+LukaUte2019@users.noreply.github.com>
2025-02-15 22:01:55 +0100

    InstaPlay

---
+++ InstaPlay/api.js
@@ -0,0 +1,40 @@
+let loadOnlineAPI = false;
+
+
+const errorElement = document.getElementById('error');
+
+$.ajax({
+	url: "http://lukaserver.ddns.net:81/instaplay",
+
+	success: function(data) {
+	errorElement.style.display = 'none';
+	}
+,
+    error: function() {
+	errorElement.style.display = 'visible';
+	}
+})
+
+export async function getSongs() {
+
+	if (loadOnlineAPI) {
+		const response = await fetch('http://lukaserver.ddns.net:81/instaplay');
+		const songs = await response.json();
+		return new Promise((resolve) => {
+			const delay = Math.floor(Math.random() * 3000) + 1000;
+			setTimeout(() => {
+				resolve(songs);
+			}, delay)
+		})
+	    
+	} else {
+		const response = await fetch('/InstaPlay/songs.json');
+		const songs = await response.json();
+		return new Promise((resolve) => {
+			const delay = Math.floor(Math.random() * 3000) + 1000;
+			setTimeout(() => {
+				resolve(songs);
+			}, delay)
+		})
+	}
+	}
